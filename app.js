dc5d17125bc425d2297e914acd14957bce3a0452
Abhi <suriabhinav1997@gmail.com>
2016-01-01 23:41:34 -0600

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+var config = require('config.json')('./sample.json');
+var login = require("facebook-chat-api");
+
+var fb_api;
+var request = require("request");
+
+
+login({email: "suriabhinav1997@gmail.com" , password: "807Crowd"}, function callback (err, api) {
+	if(err) return console.error(err);
+
+	fb_api = api
+
+	var participant_ids = [];
+	var participant_names = [];
+
+	api.listen(function callback(err, message) {
+		participant_names = message.participantNames;
+        participant_ids = message.participantIDs;   
+        if(message.type == "message") {
+				 //if @mention
+		  for (var i = 0; i < participant_names.length; i++) {
+			var name = participant_names[i].toLowerCase();
+
+			if (participant_names.length > 0) {
+				for (var i = 0; i < participant_names.length; i++) {
+					var name = String(participant_names[i]).toLowerCase();
+					if (String(message.body).toLowerCase().indexOf("@" + name) >= 0) {
+						console.log("message: " + message.body);
+						var recipient_id = "";
+						for (var i = 0; i < participant_names.length; i++) {
+							if (String(participant_names[i]).toLowerCase() == name) {
+								recipient_id = participant_ids[i];
+							}
+						}
+						console.log("recipient_id: " + recipient_id);
+                        api.getUserID(name, function(err, data) {
+                                if(err) return callback(err);
+                                var threadID = data[0].userID;
+                            api.sendMessage("You have a new message from " + message.senderName + ": \"" + message.body + "\"", threadID);
+    });
+						
+					}
+				}
+			} else {
+				console.log("received message from: " + message.senderName);
+			}
+          }
+        
+		};
+	});
+});
+
